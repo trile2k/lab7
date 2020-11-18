397180baed6c48a40635300214b178cdb146744f
trile2k <70605945+trile2k@users.noreply.github.com>
2020-11-18 00:01:17 -0600

    Fixed the Event listener handler and cleanup comments for submission

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 function init(){
   // add your javascrip between these two lines of code
-  // alert('Hello Tri');
   function click_alert() {
     // Get user input.
     var user_input = document.getElementById("entryinput").value;
-    // Overwrite content with user's input
-    document.getElementById("textoutput").innerHTML = user_input;
     // Get my name from h2 center class.
     // var name =  document.querySelector("h2.center").item(0).innerHTML;
     var name =  document.querySelector("h2.center").innerHTML;
     // Put the message together for alert.
     var msg = name + ": " + user_input;
     alert(msg); // Show alert box pops up.
+    // Overwrite content with user's input
+    document.getElementById("textoutput").innerHTML = user_input;
   }
 
   // Get element with ID attribute "entrybutton".
   var alert_me = document.getElementById('entrybutton');
   // Call click_alert function when user clicks Alert Me button.
   alert_me.addEventListener('click', click_alert);
-  // Load init function.
-  window.addEventListener('load', init);
 }
+
+// Load init function.
+window.addEventListener('load', init);
