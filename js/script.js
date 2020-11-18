ed1d752f84b0cdc52d1749db52f14d5ad3d6b22c
trile2k <70605945+trile2k@users.noreply.github.com>
2020-11-18 00:06:38 -0600

    Cleanup comments for code submission.

--- js/script.js
+++ js/script.js
@@ -4,12 +4,11 @@ function init(){
     // Get user input.
     var user_input = document.getElementById("entryinput").value;
     // Get my name from h2 center class.
-    // var name =  document.querySelector("h2.center").item(0).innerHTML;
     var name =  document.querySelector("h2.center").innerHTML;
     // Put the message together for alert.
     var msg = name + ": " + user_input;
     alert(msg); // Show alert box pops up.
-    // Overwrite content with user's input
+    // Overwrite h2 textoutput content with user's input
     document.getElementById("textoutput").innerHTML = user_input;
   }
 
