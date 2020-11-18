43c879cd37b158f6031cf861103b66878a9d424f
trile2k <70605945+trile2k@users.noreply.github.com>
2020-11-17 23:37:14 -0600

    Added a space between name and user's input.

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ function init(){
     // var name =  document.querySelector("h2.center").item(0).innerHTML;
     var name =  document.querySelector("h2.center").innerHTML;
     // Put the message together for alert.
-    var msg = name + ":" + user_input;
+    var msg = name + ": " + user_input;
     alert(msg); // Show alert box pops up.
   }
 
@@ -18,6 +18,6 @@ function init(){
   var alert_me = document.getElementById('entrybutton');
   // Call click_alert function when user clicks Alert Me button.
   alert_me.addEventListener('click', click_alert);
-
+  // Load init function.
   window.addEventListener('load', init);
 }
